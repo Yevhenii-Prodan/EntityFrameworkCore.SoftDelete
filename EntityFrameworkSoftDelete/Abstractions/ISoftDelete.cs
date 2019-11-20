8a1d7d5392b4bdd145b4e2ac70dfa82b60e08f0f
Prodan Yevhenii <yevhenii.prodan@gmail.com>
2019-11-21 00:23:39 +0300

    initial commit

---
+++ EntityFrameworkSoftDelete/Abstractions/ISoftDelete.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkSoftDelete.Abstractions
+{
+    public interface ISoftDelete
+    {
+    }
+}
---
+++ EntityFrameworkSoftDelete/Implementations/SoftDeleteDbContext.cs
@@ -0,0 +1,139 @@
+﻿using EntityFrameworkSoftDelete.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkSoftDelete.Implementations
+{
+    public class SoftDeleteDbContext : DbContext
+    {
+        private const string _isDeletedProperty = "IsDeleted";
+        private static readonly MethodInfo _propertyMethod = typeof(EF).GetMethod(nameof(EF.Property), BindingFlags.Static | BindingFlags.Public).MakeGenericMethod(typeof(bool));
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                if (typeof(ISoftDelete).IsAssignableFrom(entity.ClrType) == true)
+                {
+                    entity.AddProperty(_isDeletedProperty, typeof(bool));
+
+                    builder
+                        .Entity(entity.ClrType)
+                        .HasQueryFilter(GetIsDeletedRestriction(entity.ClrType));
+                }
+            }
+        }
+
+
+        private static LambdaExpression GetIsDeletedRestriction(Type type)
+        {
+            var parm = Expression.Parameter(type, "it");
+            var prop = Expression.Call(_propertyMethod, parm, Expression.Constant(_isDeletedProperty));
+            var condition = Expression.MakeBinary(ExpressionType.Equal, prop, Expression.Constant(false));
+            var lambda = Expression.Lambda(condition, parm);
+            return lambda;
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSaving();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            OnBeforeSaving();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        private void SetNull(EntityEntry entry, IForeignKey fk)
+        {
+            foreach (var property in fk.Properties)
+                entry.Property(property.Name).CurrentValue = null;
+        }
+
+
+        public void Restore(ISoftDelete entity)
+        {
+            var entry = ChangeTracker.Entries().First(en => en.Entity == entity);
+            if ((bool)entry.Property(_isDeletedProperty).CurrentValue == true)
+                entry.Property(_isDeletedProperty).CurrentValue = false;
+        }
+
+        public void RestoreRange(IEnumerable<ISoftDelete> entitis)
+        {
+            foreach (var entity in entitis)
+                Restore(entity);
+        }
+
+
+        private void OnBeforeSaving()
+        {
+            foreach (var entry in ChangeTracker.Entries<ISoftDelete>().ToList())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues["IsDeleted"] = false;
+                        break;
+
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues["IsDeleted"] = true;
+                        foreach (var navigationEntry in entry.Navigations.Where(n => !n.Metadata.IsDependentToPrincipal()))
+                        {
+                            if (navigationEntry is CollectionEntry collectionEntry)
+                            {
+                                collectionEntry.Load();
+                                if (collectionEntry.CurrentValue == null)
+                                    continue;
+
+                                var collection = new List<EntityEntry>();
+
+                                switch (collectionEntry.Metadata.ForeignKey.DeleteBehavior)
+                                {
+                                    case DeleteBehavior.SetNull:
+                                        foreach (var entity in collectionEntry.CurrentValue)
+                                        {
+                                            collection.Add(Entry(entity));
+                                        }
+
+                                        foreach (var dependentEntry in collection)
+                                        {
+                                            SetNull(dependentEntry, collectionEntry.Metadata.ForeignKey);
+                                        }
+                                        break;
+                                    case DeleteBehavior.Cascade:
+                                        foreach (var entity in collectionEntry.CurrentValue)
+                                            Remove(entity);
+                                        break;
+
+                                }
+                            }
+                            else
+                            {
+                                var dependentEntry = navigationEntry.CurrentValue;
+                                if (dependentEntry != null)
+                                {
+                                    Remove(Entry(dependentEntry));
+                                }
+
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+
+    }
+}
